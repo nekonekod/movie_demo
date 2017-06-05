1b61332c78e1ad6249397695798d34977718babe
nekonekod <nekonekod.goo@gmail.com>
2017-06-05 22:02:16 +0800

    grunt管理，用户注册，session保存

--- app.js
+++ app.js
@@ -4,14 +4,18 @@
 var express = require('express')
 var path = require('path')
 var bodyParser = require('body-parser')
+var session = require('express-session')
+var cookieParser = require('cookie-parser')
 var mongoose = require('mongoose')
+var mongoStore = require('connect-mongo')(session)
 var _ = require('underscore')
 var moment = require('moment')
 var Movie = require('./models/movie')
-var port = process.env.PORT || 80
+var User = require('./models/user')
+var port = process.env.PORT || 3000
 var app = express()
-
-mongoose.connect('mongodb://localhost/movie_demo')
+var dbUrl = 'mongodb://localhost/movie_demo'
+mongoose.connect(dbUrl)
 
 app.set('views', './views')
 app.set('view engine', 'jade')
@@ -21,6 +25,16 @@ app.use(bodyParser.json())
 app.locals.moment = moment
 
 app.use(express.static(path.join(__dirname, "public")))
+app.use(cookieParser())
+app.use(session({
+  secret:'Movie',
+  store:new mongoStore({
+    url:dbUrl,
+    collection:'sessions'
+  }),
+  resave: false,
+  saveUninitialized: true
+}))
 app.listen(port)
 
 console.log('movie_demo start on port ' + port)
@@ -30,9 +44,73 @@ console.log('movie_demo start on port ' + port)
 app.get('/', function (req, res) {
   Movie.fetch(function (err, movies) {
     if (err) console.log(err)
-    else res.render('pages/index', {
-      title: '首页',
-      movies: movies
+    else {
+      console.log('user in session')
+      console.log(req.session.user)
+      res.render('pages/index', {
+            title: '首页',
+            movies: movies
+          })
+    }
+  })
+})
+
+//sign up
+app.post('/user/signup',function(req,res){
+  // 1. 路由 /user/signup/:userid
+  // var _userid = req.params.userid
+  // 2. query /user/signup?userid=1111
+  // var _userid = req.query.userid
+  // 3. post-data ajax
+  // var _user = req.body.userid
+  // 或者req.param('userid')同时适用于1、2、3，优先级路由>body>query
+  var _user = req.body.user
+  //先查找是否存在同名的
+  User.find({name:_user.name},function(err,user){
+    if(err) console.log(err)
+    if(user){
+       res.redirect('/')
+    }else{
+      var user = new User(_user)
+      user.save(function(err,user){
+        if(err) console.log(err)
+        console.log(user)
+        res.redirect('/admin/user/list')
+      })
+    }
+  })
+
+})
+
+//signin 
+app.post('/user/signin',function(req,res){
+  var _user = req.body.user
+  var name = _user.name
+  var password = _user.password
+  User.findOne({name:name},function(err,user){
+    if(err) console.log(err)
+    if(!user){
+      return res.redirect('/')
+    }
+    user.comparePassword(password,function(err,isMatch){
+        if(err) console.log(err)
+        if(isMatch){
+          req.session.user = user
+          return res.redirect('/')
+        }else{
+          console.log('Password is not matched')
+        }
+    })
+  })
+})
+
+// list page
+app.get('/admin/user/list', function (req, res) {
+  User.fetch(function (err, users) {
+    if (err) console.log(err)
+    else res.render('pages/userlist', {
+      title: '用户列表',
+      users: users
     })
   })
 })
--- models/movie.js
+++ models/movie.js
@@ -1,47 +1,5 @@
 var mongoose = require('mongoose')
+var MovieSchema = require('../schema/movie')
+var Movie = mongoose.model('Movie',MovieSchema)
 
-var MovieSchema = new mongoose.Schema({
-  director:String,
-  title:String,
-  language:String,
-  country:String,
-  summary:String,
-  flash:String,
-  poster:String,
-  year:Number,
-  meta:{
-    createdAt:{
-      type:Date,
-      default:Date.now()
-    },
-    updateAt:{
-      type:Date,
-      default:Date.now()
-    }
-  }
-})
-
-MovieSchema.pre('save',function (next) {
-  if(this.isNew){
-    this.meta.createdAt = this.meta.updateAt = Date.now()
-  }else{
-    this.meta.updateAt = Date.now()
-  }
-  next()
-})
-
-MovieSchema.statics = {
-  fetch:function (cb) {
-    return this
-        .find({})
-        .sort('meta.updatedAt')
-        .exec(cb)
-  },
-  findById:function (id, cb) {
-    return this
-        .findOne({_id:id})
-        .exec(cb)
-  }
-}
-
-module.exports = mongoose.model('Movie',MovieSchema)
+module.exports = Movie
